5f652d281f5a5865a38ce9b3c8b06aa981bb83e6
James Cardinale <jms.card9.jc@gmail.com>
2020-04-27 20:11:33 -0400

    part-one-answer

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express')
-const golfMajors = require('./golfmajors')
 const app = express()
 
 app.set('view engine', 'pug')
 
-app.get('/', (request, response) => {
-  return response.render('index', { golfMajors })
+app.use(express.static('public'))
+
+app.get('/majors', (request, response) => {
+  return response.render('index')
 })
 app.get('/masters', (request, response) => {
   return response.render('masters')
+})
+app.get('/pga', (request, response) => {
+  return response.render('pga')
+})
+app.get('/usopen', (request, response) => {
+  return response.render('usopen')
+})
+app.get('/open', (request, response) => {
+  return response.render('open')
 })
 app.all('*', (request, response) => {
   return response.sendStatus(404)
